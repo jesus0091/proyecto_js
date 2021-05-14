c43b7a0c3adf8b98680956ed8619e71f2532f7db
JesusHernandez <jesushernandez120491@gmail.com>
2021-05-14 19:02:12 -0300

    desafio 3 + PF1

--- index.js
+++ index.js
@@ -1,8 +1,75 @@
-var nombre = prompt('Bienvenido, ingrese su nombre');
-var ciudad = prompt('Hola' + ' ' + nombre + ', ¿En qué ciudad te encuentras?');
-
-if ((ciudad.toUpperCase()).trim() == ('CABA' || 'BUENOS AIRES')) {
-        alert('Bienvenido, hay turnos disponibles en su ubicación');
-    } else {
-        alert('El servicio solo esta disponible en Buenos Aires');
+var cliente1 = {
+    nombre: prompt("Cliente 1: Ingrese su nombre:"),
+    apellido: prompt("Ingrese su apellido:"),
+    correo: prompt("Ingrese su correo electrónico:"),
+    telefono: prompt("Ingrese tu telefono:"),
+    pelo: prompt("Quiere corte de pelo?"),
+    barba: prompt("Quiere corte de barba?"),
+    servicio: function(){
+        var preciocorte = 500;
+        var preciobarba = 200;
+        if( (this.pelo.toUpperCase()).trim() == "SI" && (this.barba.toUpperCase()).trim() == "SI" ){
+            console.log("Debe pagar: " + (preciocorte + preciobarba));
+        }else{
+            console.log("Debe pagar: " + preciocorte);
+        }
     }
+}
+var cliente2 = {
+    nombre: prompt("Cliente 2: Ingrese su nombre:"),
+    apellido: prompt("Ingrese su apellido:"),
+    correo: prompt("Ingrese su correo electrónico:"),
+    telefono: prompt("Ingrese tu telefono:"),
+    pelo: prompt("Quiere corte de pelo?"),
+    barba: prompt("Quiere corte de barba?"),
+    servicio: function(){
+        var preciocorte = 500;
+        var preciobarba = 200;
+        if( (this.pelo.toUpperCase()).trim() == "SI" && (this.barba.toUpperCase()).trim() == "SI" ){
+            console.log("Debe pagar: " + (preciocorte + preciobarba));
+        }else{
+            console.log("Debe pagar: " + preciocorte);
+        }
+    }
+}
+var cliente3 = {
+    nombre: prompt("Cliente 3: Ingrese su nombre:"),
+    apellido: prompt("Ingrese su apellido:"),
+    correo: prompt("Ingrese su correo electrónico:"),
+    telefono: prompt("Ingrese tu telefono:"),
+    pelo: prompt("Quiere corte de pelo?"),
+    barba: prompt("Quiere corte de barba?"),
+    servicio: function(){
+        var preciocorte = 500;
+        var preciobarba = 200;
+        if( (this.pelo.toUpperCase()).trim() == "SI" && (this.barba.toUpperCase()).trim() == "SI" ){
+            console.log("Debe pagar: " + (preciocorte + preciobarba));
+        }else{
+            console.log("Debe pagar: " + preciocorte);
+        }
+    }
+}
+var cliente4 = {
+    nombre: prompt("Cliente 4: Ingrese su nombre:"),
+    apellido: prompt("Ingrese su apellido:"),
+    correo: prompt("Ingrese su correo electrónico:"),
+    telefono: prompt("Ingrese tu telefono:"),
+    pelo: prompt("Quiere corte de pelo?"),
+    barba: prompt("Quiere corte de barba?"),
+    servicio: function(){
+        var preciocorte = 500;
+        var preciobarba = 200;
+        if( (this.pelo.toUpperCase()).trim() == "SI" && (this.barba.toUpperCase()).trim() == "SI" ){
+            console.log("Debe pagar: " + (preciocorte + preciobarba));
+        }else{
+            console.log("Debe pagar: " + preciocorte);
+        }
+    }
+}
+var clientes = [];
+clientes.push(cliente1, cliente2, cliente3, cliente4); 
+
+for(i=0; i < clientes.length; i++){
+    clientes[i].servicio();
+
+}
